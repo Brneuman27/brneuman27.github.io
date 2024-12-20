29f384f314e106fb18934c6332ce3ffd93f0b291
Brandon Neuman <brandon.neuman27@student.tamdistrict.org>
2024-12-20 09:20:08 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+var cWidth = 3000; 
+var cHeight = 3000; 
 
+var fish; 
+
+function setup() { 
+  createCanvas(cWidth, cHeight); 
+  colorMode(HSB, 100); 
+  fish = new Fish(500, 250); 
+  console.log(fish); 
+} 
+
+function draw() { 
+  background(65, 70, 70); 
+  fish.drawSelf(); 
+  fish.move(); 
+} 
+
+function placeFree(obj, x, y) { 
+  if (x - obj.size/2 < 0) { return false; } 
+  if (x + obj.size/2 > cWidth) { return false; } 
+  if (y - obj.size/2 < 0) { return false; } 
+  if (y + obj.size/2 > cHeight) { return false; } 
+  
+  return true; 
+}
